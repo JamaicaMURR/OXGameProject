296fa696954c5134f58e772da84d2a0887420ed0
murr123yandex.ru <murr123yandex.ru@outlook.com>
2021-05-13 00:33:44 +0300

    StartPoint

---
+++ Assets/Scripts/X_Mover.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class X_Mover : MonoBehaviour
+{
+
+    public float netSize = 1.5f;
+
+    public float netX = 0;
+    public float netY = 0;
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+}
