052d9c420e7de454f88a7083357fabe0f23170c3
murr123yandex.ru <murr123yandex.ru@outlook.com>
2021-05-15 17:02:32 +0300

    SuitChanger script added

---
+++ Assets/Scripts/SuitChanger.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SuitChanger : MonoBehaviour
+{
+    static System.Random random = new System.Random();
+
+    SpriteRenderer _spriteRenderer;
+
+    public Sprite[] suits;
+
+    public bool changeAtStart = true;
+    public bool randomFlipWhenChange = true;
+
+    void Awake()
+    {
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+
+        if(_spriteRenderer == null)
+            throw new System.Exception("Can't find SpriteRenderer component");
+
+        if(suits.Length == 0)
+            throw new System.Exception("No suits");
+    }
+
+    void Start()
+    {
+        if(changeAtStart)
+            ChangeSuit();
+    }
+
+    public void ChangeSuit(int number)
+    {
+        _spriteRenderer.sprite = suits[number];
+
+        if(randomFlipWhenChange)
+        {
+            _spriteRenderer.flipX = random.NextDouble() < 0.5;
+            _spriteRenderer.flipY = random.NextDouble() < 0.5;
+        }
+    }
+
+    public void ChangeSuit()
+    {
+        ChangeSuit(random.Next(0, suits.Length));
+    }
+}
