b357b7cf925c14e9966f0bd2588d97961bebb5c9
murr123yandex.ru <murr123yandex.ru@outlook.com>
2021-05-18 01:23:32 +0300

    Pausers mechanic realised

--- Assets/Scripts/Masters/CentralPort.cs
+++ Assets/Scripts/Masters/CentralPort.cs
@@ -5,6 +5,7 @@ public class CentralPort : MonoBehaviour
 {
     public NetMaster netMaster;
     public HeartsMaster heartsMaster;
+    public HeartsMaster pausersMaster;
     public MergeMaster mergeMaster;
     public SpawnMaster spawnMaster;
 }
--- Assets/Scripts/Masters/FieldInputHandler.cs
+++ Assets/Scripts/Masters/FieldInputHandler.cs
@@ -1,11 +1,21 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System;
 
 public class FieldInputHandler : MonoBehaviour
 {
     bool isPaused = false;
+    bool isPauserUsed = false;
 
+    Action Check;
+
+    public CentralPort port;
     public XBehavior xBehavior;
+
+    void Awake()
+    {
+        Check = Idle;
+    }
 
     void Update()
     {
@@ -14,21 +24,55 @@ public class FieldInputHandler : MonoBehaviour
             isPaused = !isPaused;
 
             if(isPaused)
+            {
                 Time.timeScale = 0;
+                isPauserUsed = false;
+                Check = CheckPauser;
+            }
             else
+            {
                 Time.timeScale = 1;
+                Check = Idle;
+            }
         }
 
-        if(Input.GetButtonDown("Up"))
-            xBehavior.TryToMove(Direction.Up);
+        if(!isPaused || isPauserUsed || port.pausersMaster.Hearts > 0)
+        {
+            if(Input.GetButtonDown("Up"))
+            {
+                xBehavior.TryToMove(Direction.Up);
+                Check();
+            }
 
-        if(Input.GetButtonDown("Down"))
-            xBehavior.TryToMove(Direction.Down);
+            if(Input.GetButtonDown("Down"))
+            {
+                xBehavior.TryToMove(Direction.Down);
+                Check();
+            }
 
-        if(Input.GetButtonDown("Left"))
-            xBehavior.TryToMove(Direction.Left);
+            if(Input.GetButtonDown("Left"))
+            {
+                xBehavior.TryToMove(Direction.Left);
+                Check();
+            }
 
-        if(Input.GetButtonDown("Right"))
-            xBehavior.TryToMove(Direction.Right);
+            if(Input.GetButtonDown("Right"))
+            {
+                xBehavior.TryToMove(Direction.Right);
+                Check();
+            }
+        }
     }
+
+    void CheckPauser()
+    {
+        if(isPaused && !isPauserUsed)
+        {
+            port.pausersMaster.Hearts--;
+            isPauserUsed = true;
+            Check = Idle;
+        }
+    }
+
+    void Idle() { }
 }
