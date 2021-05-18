236b5c740753d330a1afe8fddc5406a172657bb8
murr123yandex.ru <murr123yandex.ru@outlook.com>
2021-05-18 11:31:17 +0300

    Colorator added

--- Assets/Scripts/Colorator.cs
+++ Assets/Scripts/Colorator.cs
@@ -1,18 +1,38 @@
 ﻿using UnityEngine;
 using System.Collections;
+using Bycicles.Ranges;
 
 public class Colorator : MonoBehaviour
 {
+    SpriteRenderer _rendrer;
+    Color _initialColor;
 
-    // Use this for initialization
-    void Start()
+    public Color targetColor;
+
+    void Awake()
     {
-
+        _rendrer = GetComponent<SpriteRenderer>();
+        _initialColor = _rendrer.color;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Paint(float exponent)
     {
+        exponent.ExNotBelow(0, "Color exponent").ExNotAbove(1, "Color exponent");
 
+        float r = (targetColor.r - _initialColor.r) * exponent + _initialColor.r;
+        float g = (targetColor.g - _initialColor.g) * exponent + _initialColor.g;
+        float b = (targetColor.b - _initialColor.b) * exponent + _initialColor.b;
+
+        _rendrer.color = new Color(r, g, b);
+    }
+
+    public void Paint()
+    {
+        Paint(1);
+    }
+
+    public void Reset()
+    {
+        _rendrer.color = _initialColor;
     }
 }
